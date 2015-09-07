98919d0a195420850fa03b2d123d3e4bc5e9ef97
Juan <juaniisuar@gmail.com>
2015-09-07 16:02:24 -0300

    Update tests.cpp

--- tests.cpp
+++ tests.cpp
@@ -1,1 +1,12 @@
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+#include "catch.hpp"
+#include "Movilidad.cpp"
+
+TEST_CASE("holi", "[factorial]"){
+  REQUIRE( 5 == 4);
+  REQUIRE( 0 == 0);
+  REQUIRE( 0 == NULL);
+  
+  
+}
 
